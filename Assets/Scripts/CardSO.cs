2a911a3789e773ad3e228f713001c3ee8d1f2860
clcic23 <claudiu.ciceu23@gmail.com>
2026-01-17 23:09:04 +0200

    card
    card

---
+++ Assets/Scripts/CardSO.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class CardSO : ScriptableObject
+{
+    public Sprite cardImage;
+    public string cardText;
+    public CardEffect effectType;
+    public float effectValue; // Cât damage/viață dă cardul
+    public bool isUnique;    // Dacă apare o singură dată
+    public int unlockLevel;  // La ce nivel se deblochează
+}
+
+public enum CardEffect
+{
+    DamageIncrease
+}
